7ae94f3e1308d1fcd957d3f7a03cfeceb666d140
Data & Ljud <johan@dataochljud.se>
2018-09-03 23:16:23 +0200

    Fixat vart du skickas efter "Klar"
    "Klar" skickar upp dig en nivå istället för till index.php. Samma som bokning.

--- PPval2/sklar.php
+++ PPval2/sklar.php
@@ -1,11 +1,17 @@
 <?php
 $lokalkod = $_GET['lokal'];
-
+$lkod = $lokalkod;
+//echo $lkod . "<br>";
+$elk = explode('_',$lkod);
+$lk = $lkod;
+$lan = $elk[0];
+$kommun = $elk[1];
 require('open_database.php');
 
 $sql = 'UPDATE vallokal SET Status = "K" where LokalKod="' . $lokalkod . '"'; 
 
 if (!$conn->query($sql)) { echo "Databasfel!"; }
+header('Location: https://www.johantibbelin.se/ppval2/fget_lokaler.php?lan=' . $lan . '&kommun=' . $kommun);
+exit();
 
-require('index.php');
 ?>
