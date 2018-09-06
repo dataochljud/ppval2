a8743cce8f5bd664021f99126d9e2a68993aca98
Data & Ljud <johan@dataochljud.se>
2018-09-06 13:21:38 +0200

    Fixed database connection

--- PPval2/includes/open_database.php
+++ PPval2/includes/open_database.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "johantibbelin.se.mysql";
+$username = "johantibbelin_se_ppval";
+$password = "ppval2018";
 $dbname = "johantibbelin_se_ppval";
 
 // Create connection
